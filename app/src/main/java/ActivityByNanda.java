9016c9cdc6f38ef5261d72e8b291fc15733f8fe6
Nanda <anandaadnilia@gmail.com>
2019-11-15 12:25:18 +0700

    test1

---
+++ app/src/main/java/ActivityByNanda.java
@@ -0,0 +1,12 @@
+package com.example.tutorialbynanda;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityByNanda extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_nanda);
+    }
+}
